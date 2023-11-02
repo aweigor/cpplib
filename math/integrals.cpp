4639aa13bbb63eb060ba14daa25de6f3015cb311
igor <drmnnml@gmail.com>
2023-11-02 13:12:43 +0300

    integrals correction errors, debug/refactor

--- math/integrals.cpp
+++ math/integrals.cpp
@@ -1,24 +1,69 @@
 # include <iostream>
 # include <cmath>
 
-int midpoint(int l, int r, int (*func) (int)) {
-  int ret;
-  // step of integration
-  int step = r - l / 2;
-  // sum of heights
-  for (int i = 1; i < r; i++) {
-    ret += func(i - (step / 2));
+/*
+ * Range of Integration
+ **/
+struct IntegrationRange {
+  float from;
+  float to;
+  int precision;
+};
+
+struct IntegrationResult {
+  float integral;
+  float error;
+};
+
+/*
+ * Integration by the method of midpoint rectangles
+ **/
+int midpoint(IntegrationRange r, float (*y) (float)) 
+{
+  float integral = 0;
+  float step = ( r.to - r.from ) / r.precision;
+  
+  for (float x = r.from; x <= r.to; x += step) {
+    integral += y(x - (step / 2));
   }
-  // return multiplication of sums
-  std::cout << step << std::endl;
-  return ret * step;
+  
+  integral *= step;
+
+  return integral;
 }
 
-int y (int x) {
+/*
+ * Calculation endpoint
+ **/
+IntegrationResult integrate(
+  float (*y) (float),
+  float from,
+  float to,
+  int precision,
+  int mode
+) {
+  IntegrationRange r1 = { from, to, precision };
+  IntegrationRange r2 = { from, to, precision * 2 };
+
+  float integral1n = midpoint(r1, y);
+  float integral2n = midpoint(r2, y);
+
+  return (IntegrationResult){ integral1n, integral2n - integral1n };
+}
+
+/*
+ * Test function
+ **/
+float y (float x) 
+{
   return std::pow(2, x);
 }
 
 int main() 
 {
-  std::cout << midpoint(0, 10, &::y) << std::endl;
+  IntegrationResult i1 = integrate(::y, 1.0, 10.0, 20, 0);
+  std::cout << "Result: " << i1.integral << std::endl;
+  std::cout << "Error: " << i1.error << std::endl;
+
+  return 0;
 }
